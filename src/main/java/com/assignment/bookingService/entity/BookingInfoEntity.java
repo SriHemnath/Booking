a698489014b5c055b67ea16bfc378dda9829728d
Hemnath J <hemnath.j1995@gmail.com>
2024-09-29 22:44:22 +0530

    javax -> jakarta

--- src/main/java/com/assignment/bookingService/entity/BookingInfoEntity.java
+++ src/main/java/com/assignment/bookingService/entity/BookingInfoEntity.java
@@ -2,7 +2,13 @@ package com.assignment.bookingService.entity;
 
 import org.hibernate.annotations.ColumnDefault;
 
-import javax.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.util.Date;
 
 @Entity
